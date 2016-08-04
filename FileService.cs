8c7ce211734d03617b24bdd2819992bd468db76a
Debasis Chakraborty <debasis.yours@gmail.com>
2016-08-05 02:26:51 +0530

    Initial commit

---
+++ FileService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileWatcherService
+{
+    public partial class FileService : ServiceBase
+    {
+        public FileService()
+        {
+            InitializeComponent();
+            ServiceBase[] servicesToRun = new ServiceBase[]{new FileService()};
+            ServiceBase.Run(servicesToRun);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            EventLog.WriteEntry("FileWatcher started", EventLogEntryType.Information);
+        }
+
+        protected override void OnStop()
+        {
+            EventLog.WriteEntry("FileWatcher stopped", EventLogEntryType.Information);
+        }
+    }
+}
